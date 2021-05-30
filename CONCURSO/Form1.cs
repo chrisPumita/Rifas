d9bccf0cb4b59314e6b2de20023a9c24a73c46a8
Christan RCSG <christian.floppy@gmail.com>
2021-05-30 14:43:49 -0500

    VERSION FINAL
    Sistema Completo, minimo de compra $30 MXN

---
+++ CONCURSO/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CONCURSO
+{
+    public partial class Form1 : Form
+    {
+        decimal MIN_COMRPA = 30;
+        PrintPageEventArgs e;
+
+        string _nombre;
+        string _folio;
+        string _monto;
+        bool _corte;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cboNombres.SelectedIndex = 0;
+            limpia();
+        }
+
+        private void limpia()
+        {
+            txtNombre.Clear();
+            numCompra.Value = numCompra.Minimum;
+            txtNombre.Focus();
+            this.Text = "ReCkrea 1er Aniversario - " + Properties.Settings.Default.folio + " boletos generados";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            generarBoleto();
+        }
+
+        private void txtNombre_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                numCompra.Focus();
+            }
+        }
+
+        private void numCompra_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                generarBoleto();
+            }
+        }
+
+        private void generarBoleto() 
+        {
+            if (!string.IsNullOrEmpty(txtNombre.Text))
+            {
+                if (numCompra.Value >= MIN_COMRPA)
+                {
+                    decimal compra = numCompra.Value;
+                    int NoTicket = Convert.ToInt32(Math.Floor(compra / MIN_COMRPA));
+
+                    for (int i = 0; i < NoTicket; i++)
+                    {
+                        if (addCliente(txtNombre.Text, numCompra.Value))
+                        {
+                            //imprimir boleto
+                            this._corte = false;
+                            this._nombre = txtNombre.Text;
+                            this._monto = numCompra.Value.ToString();
+                            this._folio = Properties.Settings.Default.folio.ToString();
+
+                            if ((i+1)!=NoTicket)
+                                this._corte = true;
+                            printDocument1 = new PrintDocument();
+                            PrinterSettings ps = new PrinterSettings();
+                            //  elejir la impresora a donde se va a imprimir
+                            ps.DefaultPageSettings.PrinterSettings.PrinterName = Properties.Settings.Default.printerTicket;
+                            printDocument1.PrinterSettings = ps;
+                            printDocument1.PrintPage += imprimirBoleto;
+                            printDocument1.Print();
+                            MessageBox.Show("Porfavor corte el boleto " + (i+1) + " de "+ NoTicket);
+                        }
+                    }
+                    limpia();
+                }
+                else
+                {
+                    MessageBox.Show("El minimo de compra debe ser de $" + MIN_COMRPA);
+                    numCompra.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Porfavor escriba el nombre del participante");
+                txtNombre.Focus();
+            }
+
+        }
+
+
+        //// FUNCIONES PRINCIPALES
+        public bool addCliente(string nombre, decimal monto)
+        {
+            try 
+	        {	       
+                StreamWriter fichero;
+                int folio = consultaFolioAnterior() + 1 ;
+                //fichero = File.CreateText("registroConcurso.txt");
+                //fichero.WriteLine(consultaFolioAnterior()+"\t$"+monto+"\t"+nombre);
+                //fichero.Close();
+                fichero = File.AppendText("registroConcurso.txt");
+                fichero.WriteLine("" + cboNombres .SelectedItem.ToString()+ "\t" + folio + "\t$" + monto + "\t" + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString() + "\t" + nombre);
+                fichero.Close();
+                guardaFolioNuevo(folio);
+                return true;
+	        }
+	        catch (Exception)
+	        {
+		        return false;
+	        }
+        }
+
+        private int consultaFolioAnterior()
+        {
+            return Properties.Settings.Default.folio;
+        }
+        private void guardaFolioNuevo(int folio)
+        {
+            Properties.Settings.Default.folio = folio;
+            Properties.Settings.Default.Save();
+        }
+
+        private void imprimirBoleto(object sender, PrintPageEventArgs e)
+        {
+            IMPRESORA printer = new IMPRESORA();
+            printer.imprimir(_nombre, _monto, _folio, _corte, e);
+        }
+    }
+
+
+}
---
+++ CONCURSO/IMPRESORA.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Globalization;
+
+namespace CONCURSO
+{
+    class IMPRESORA
+    {
+        Font _fontdetalle = new Font("Consolas", 8, FontStyle.Regular, GraphicsUnit.Point);
+        Font _fontTOTAL = new Font("Consolas", 8, FontStyle.Bold, GraphicsUnit.Point);
+        Font font = new Font("Arial", 8, FontStyle.Regular, GraphicsUnit.Point);
+        Font fontBlack = new Font("Arial", 8, FontStyle.Bold, GraphicsUnit.Point);
+        Font fontCursive = new Font("Arial", 7, FontStyle.Italic, GraphicsUnit.Point);
+        Font fontFolio = new Font("Consolas", 25, FontStyle.Bold, GraphicsUnit.Point);
+        Font fontFolioGde = new Font("Consolas", 45, FontStyle.Bold, GraphicsUnit.Point);
+        Font terminos = new Font("Arial", 7, FontStyle.Italic, GraphicsUnit.Point);
+        Font obligaciones = new Font("Consolas", 5, FontStyle.Italic, GraphicsUnit.Point);
+        int ancho = Convert.ToInt32(Convert.ToDouble(Properties.Settings.Default.printerWidth) * 3.779527559055);
+        int y = 5;
+        int marginLeft = 0;
+        int interlineado = 12;
+        Image img = Properties.Resources.image;
+
+        public void imprimir(string nombre, string monto, string folio, bool corte, PrintPageEventArgs e)
+        {
+            //Colocar el icono predefinido
+            e.Graphics.DrawImage(img, new Rectangle(interlineado+20, 0 , (85), 46));
+            y += (interlineado * 3);
+            e.Graphics.DrawString("**Rifa Anual 1er Aniversario**", fontBlack, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString("  " + folio + "", fontFolio, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado * 5));
+            e.Graphics.DrawString("______                     ______", fontBlack, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            y += interlineado * 1;
+            e.Graphics.DrawString("GRACIAS " + nombre, fontCursive, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString("Por tu compra de: $" + monto, fontCursive, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString(DateTime.Now.ToShortDateString()+" " + DateTime.Now.ToShortTimeString(), _fontdetalle, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+
+            e.Graphics.DrawString("", terminos, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString("-Rifaremos regalos el DÍA:", terminos, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString(" VIERNES 18 DE JUNIO A LA 1:00 PM", terminos, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString("-Para conocer el ganador sigue nuestras", terminos, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString("  historias en WhatsApp (55 1080 1566)", terminos, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString(" o visitanos; publicaremos al ganador(a)", terminos, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString(" en la entrada de ReCkrea CyberCafé", terminos, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            
+            e.Graphics.DrawString("      *** MUCHA SUERTE :) ***", fontBlack, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString(" == Gracias por tu preferencia ==", fontBlack, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString("IMPORTANTE: Es necesario presentar este ", obligaciones, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString("   boleto para reclamar el premio", obligaciones, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString("", fontBlack, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString("8<---------------------------------------", _fontTOTAL, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString("", terminos, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawImage(img, new Rectangle(interlineado + 80, y+=interlineado, (85), 46));
+            e.Graphics.DrawString(folio + "", fontFolio, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado * 5));
+            e.Graphics.DrawString("______                     ______", fontBlack, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString("", fontCursive, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString("" + nombre, fontCursive, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString("Compra: $" + monto, fontCursive, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            e.Graphics.DrawString(DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString(), _fontdetalle, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+            if(corte)
+                e.Graphics.DrawString("8<---------------------------------------", _fontTOTAL, Brushes.Black, new RectangleF(marginLeft, y += interlineado, ancho, interlineado));
+        }
+
+    }
+}
